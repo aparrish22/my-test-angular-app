3e228e48e04c478f3e9220666bfbbfb4e008cd73
aparrish22 <austinparrish202@gmail.com>
2024-02-01 20:27:26 -0600

    updated components, attempt to push repo

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { SocialIconsComponent } from './social-icons/social-icons.component';  // Adjust the path accordingly
+
+@NgModule({
+    declarations: [
+    // other components,
+        
+    ],
+    imports: [
+        SocialIconsComponent,
+    ],
+    // other module configurations
+})
+export class AppModule {}
